5d4432d2fc3a7d053a1983bbccdc8cee676a2ca3
Henrique Lopes <henriqueborsoilopes@gmail.com>
2021-01-31 18:52:24 -0300

    Feat: start text robot

---
+++ robots/text.js
@@ -0,0 +1,58 @@
+const algorithmia = require('algorithmia')
+const algorithmiaApiKey = require('../credentials/algorithmia.json').apiKey
+const sentenceBoundaryDetection = require('sbd')
+
+async function robot(content) {
+    await fetchContentFromWikipedia(content)
+    sanitizaContent(content)
+    breakContentIntoSentences(content)
+
+    async function fetchContentFromWikipedia(content) {
+        const algorithmiaAuthenticated = algorithmia(algorithmiaApiKey)
+        const wikipediaAlgorithm = algorithmiaAuthenticated.algo('web/WikipediaParser/0.1.2')
+        const wikipediaResponse = await wikipediaAlgorithm.pipe(content.searchTerm)
+        const wikipediaContent = wikipediaResponse.get()
+        
+        content.sourceContentOriginal = wikipediaContent.content
+    }
+
+    function sanitizaContent(content) {
+        const withoutBlankLinesAndMarkdown = removeBlankLinesAndMarkdown(content.sourceContentOriginal)
+        const withoutDateIntoSentences = removeDateInParentheses(withoutBlankLinesAndMarkdown)
+
+        content.sourceContentSanitized =  withoutDateIntoSentences
+
+        function removeBlankLinesAndMarkdown(text) {
+            const allLines = text.split('\n')
+
+            const withoutBlankLinesAndMarkdown = allLines.filter((line) => {
+                if (line.trim().length === 0 || line.trim().startsWith('=')) {
+                    return false
+                }
+                return true
+            })
+            return withoutBlankLinesAndMarkdown.join(' ')
+        }
+    }
+
+    function removeDateInParentheses(text) {
+        return text.replace(/\((?:\([^()]*\)|[^()])*\)/gm, '').replace(/  /g,' ')
+    }
+
+    function breakContentIntoSentences(content) {
+        content.sentences = []
+
+        const sentences = sentenceBoundaryDetection.sentences(content.sourceContentSanitized)
+        sentences.forEach((senteces) => {
+            content.sentences.push({
+                text: senteces,
+                keywords: [],
+                images: []
+            })
+        })
+
+
+    }
+}
+
+module.exports = robot
